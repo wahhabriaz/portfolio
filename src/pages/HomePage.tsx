b2c39df982b5de5196fce78e1ab5a62b7f05cb9f
wahhabriaz <93725258+wahhabriaz@users.noreply.github.com>
2026-01-09 00:01:30 +0500

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,12 +5,13 @@ import React from "react";
 import WorkSection from "../components/WorkSection";
 import Hero from "../components/Hero";
 import FirstStorySection from "../components/FirstStorySection";
+import HomeIntro from "../components/HomeIntro";
 
 export default function HomePage() {
   return (
     <>
     <Hero/>
- 
+ <HomeIntro/>
     <FirstStorySection/>
       <WorkSection />
     </>
