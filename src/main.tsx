902f829df1c6bac6dd1ba717c1a0fccd6caedabf
wahhabriaz <93725258+wahhabriaz@users.noreply.github.com>
2026-01-10 05:58:50 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
+import SmoothScrollProvider from './providers/SmoothScrollProvider';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-     <App />
+  <SmoothScrollProvider>
+ <App />
+  </SmoothScrollProvider>
+    
     </BrowserRouter>
    
   </React.StrictMode>
