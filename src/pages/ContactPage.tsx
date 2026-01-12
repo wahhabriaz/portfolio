3e53d13a368f3359dd93a8f60e46f8ade63f6778
wahhabriaz <93725258+wahhabriaz@users.noreply.github.com>
2026-01-13 00:27:08 +0500

    Create ContactPage.tsx

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WorkSection from "../components/WorkSection";
+import Contact from "../components/Contact";
+
+export default function ContactPage() {
+  return (
+    <>
+    <Contact/>
+    
+    </>
+  );
+}
